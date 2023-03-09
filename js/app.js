59cf9d8bbac3ce12c2604e9db7a73b72ae8fcc8a
Agito-debug <evita10@abv.bg>
2023-03-09 21:23:59 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    // margin:10,   // since one item ou can remove it
+    nav:false,
+    // dots:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+
+
+});
+  });
